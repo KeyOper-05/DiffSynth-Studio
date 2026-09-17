0c7f8934d924472ed9ce73f6676d2f9f14601dce
KeyOper-05 <177803238+KeyOper-05@users.noreply.github.com>
2026-09-17 19:20:39 +0800

    rm irrelevant inference scripts

--- examples/wanvideo/model_training/lora/StoryMem-Wan2.2-prefuse-lora.sh
+++ examples/wanvideo/model_training/lora/StoryMem-Wan2.2-prefuse-lora.sh
@@ -1,7 +1,7 @@
 python examples/wanvideo/model_training/lora/prefuse_storymem_lora.py \
-  --base_shards "../Wan2.2_Pretrained/i2v/high_noise_model/diffusion_pytorch_model-*.safetensors" \
-  --lora_path "../StoryMem_Pretrained/Wan2.2-MI2V-A14B/backbone_high_noise.safetensors" \
-  --output_dir "./models/storymem_prefused/Wan2.2-MI2V-A14B/high_noise_model" \
+  --base_shards "../../checkpoints/Wan2.2_Pretrained/i2v/high_noise_model/diffusion_pytorch_model-*.safetensors" \
+  --lora_path "../../checkpoints/StoryMem_Pretrained/Wan2.2-MI2V-A14B/backbone_high_noise.safetensors" \
+  --output_dir "../../checkpoints/storymem_prefused/Wan2.2-MI2V-A14B/high_noise_model" \
   --lora_alpha 128 \
   --lora_rank 128 \
   --rslora \
@@ -9,9 +9,9 @@ python examples/wanvideo/model_training/lora/prefuse_storymem_lora.py \
   --compute_dtype bf16
 
 python examples/wanvideo/model_training/lora/prefuse_storymem_lora.py \
-  --base_shards "../Wan2.2_Pretrained/i2v/low_noise_model/diffusion_pytorch_model-*.safetensors" \
-  --lora_path "../StoryMem_Pretrained/Wan2.2-MI2V-A14B/backbone_low_noise.safetensors" \
-  --output_dir "./models/storymem_prefused/Wan2.2-MI2V-A14B/low_noise_model" \
+  --base_shards "../../checkpoints/Wan2.2_Pretrained/i2v/low_noise_model/diffusion_pytorch_model-*.safetensors" \
+  --lora_path "../../checkpoints/StoryMem_Pretrained/Wan2.2-MI2V-A14B/backbone_low_noise.safetensors" \
+  --output_dir "../../checkpoints/storymem_prefused/Wan2.2-MI2V-A14B/low_noise_model" \
   --lora_alpha 128 \
   --lora_rank 128 \
   --rslora \
